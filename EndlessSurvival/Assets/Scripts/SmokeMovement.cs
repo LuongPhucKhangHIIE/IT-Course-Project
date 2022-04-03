4424d80b50508dfc7c7895e455d7fc782628b87f
LuongPhucKhangHIIE <71858455+LuongPhucKhangHIIE@users.noreply.github.com>
2022-04-03 13:54:15 +0700

    First Upload

---
+++ EndlessSurvival/Assets/Scripts/SmokeMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SmokeMovement : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (vertical)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y+speed*Time.deltaTime ,0);
+            if (transform.position.y > 112)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, 0);
+            if (transform.position.x > 112)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
